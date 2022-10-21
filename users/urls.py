b8e7b232f88365333eb45293b0f404167517cb3f
Polydexter <antonverevkine@gmail.com>
2022-10-21 20:44:30 +0600

    Integrated authentication with frontend

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RegisterAPI
+from .views import RegisterAPI, UserDetails
 
 
 urlpatterns = [
     path('register/', RegisterAPI.as_view()),
+    path('details/', UserDetails.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, permissions, mixins
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.tokens import AccessToken
 from .serializers import RegisterSerializer, UserSerializer
 from django.contrib.auth.models import User
 
@@ -16,4 +17,16 @@ class RegisterAPI(generics.GenericAPIView):
             "user": UserSerializer(user,
             context=self.get_serializer_context()).data,
             "message": "User created successfully. Now log in to get token",
+        }, status=201)
+
+class UserDetails(generics.GenericAPIView):
+    permission_classes = (IsAuthenticated,)
+    def get(self, request):
+        token = request.headers['Authorization'].split(' ')[1]
+        access_token = AccessToken(token)
+        user = User.objects.get(pk=access_token['user_id'])
+        message = "Hello, authenticated user!"
+        return Response({
+            'user': UserSerializer(user,
+            context=self.get_serializer_context()).data,
         })
